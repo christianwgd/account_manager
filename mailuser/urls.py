61dda8c626e5f1f7f93594af709c8ed8ddf98949
Christian Wiegand <cwiegand@wgdnet.de>
2019-04-29 18:54:37 +0200

    Update fa, change admin and tfa icons

--- mailuser/urls.py
+++ mailuser/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.views.generic import RedirectView
 from filebrowser.sites import site
 from django.conf import settings
