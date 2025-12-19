a088ca77898857fab5969c18a3870575aabf72b3
Christian Wiegand <christianwgd@users.noreply.github.com>
2025-12-19 16:49:55 +0100

    Refactor search, remove js and migrate to filter

--- account/filters.py
+++ account/filters.py
@@ -5,14 +5,21 @@ from django.db.models import Q
 from account.models import Account
 
 
-class CompanyFilter(django_filters.FilterSet):
+class AccountFilter(django_filters.FilterSet):
     class Meta:
         model = Account
-        fields = [
-            'name', 'type'
-        ]
+        fields = ['search']
 
-    author = django_filters.CharFilter(
-        lookup_expr='icontains',
-        label=_('Name contains')
+    search = django_filters.CharFilter(
+        method='search_all',
+        label=_('Search')
     )
+
+    def search_all(self, queryset, name, value):
+        return queryset.filter(
+            Q(name__icontains=value) |
+            Q(username__icontains=value) |
+            Q(first_name__icontains=value) |
+            Q(last_name__icontains=value) |
+            Q(description__icontains=value)
+        )
